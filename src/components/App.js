fb7949db916ad1ecdfa349df1e272af454d3b8c0
MinChan Lee <shigatsu970704@gmail.com>
2021-01-21 11:12:06 +0900

    01.Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import GlobalStyle from "./GlobalStyles";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/:id" component={Detail} />
+      </Router>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
