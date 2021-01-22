f4a2bf9f8d3726c9b5aea3856146b2f044e6175e
MinChan Lee <shigatsu970704@gmail.com>
2021-01-22 12:10:55 +0900

    07.Local State

--- src/apollo.js
+++ src/apollo.js
@@ -3,6 +3,22 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.modify({
+          id: `Movie:${id}`,
+          fields: {
+            isLiked: () => true,
+            medium_cover_image: () => "lalaallaa",
+          },
+        });
+      },
+    },
+  },
 });
 
 export default client;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,18 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
 
 const Container = styled.div`
   height: 380px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
   border-radius: 7px;
 `;
 
@@ -18,12 +24,18 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
-export const Movie = ({ id, bg }) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={bg} />
-    </Link>
-  </Container>
-);
+export const Movie = ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, { variables: { id: +id } });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={isLiked ? null : likeMovie}>
+        {isLiked ? "Unlike" : "Like"}
+      </button>
+    </Container>
+  );
+};
 
 export default Movie;
